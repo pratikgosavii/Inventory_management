c25d02c247da3aada29061376e47fb0e904ea494
pratik gosavi <58395315+pratikgosavii@users.noreply.github.com>
2023-05-31 19:11:52 +0530

    sd

--- transactions/views.py
+++ transactions/views.py
@@ -49,7 +49,7 @@ def demo(request):
 
         a = inward_fi.filter(company__company_name = ab.company.company_name, company_goods__name = ab.company_goods.name, goods_company__goods_company_name = ab.goods_company.goods_company_name, DC_date__gte = date(int(2023), int(4), int(1)), DC_date__lte = date(int(2023), int(5), int(31)))
         b = outward_fi.filter(company__company_name =  ab.company.company_name, company_goods__name = ab.company_goods.name, goods_company__goods_company_name = ab.goods_company.goods_company_name, DC_date__gte = date(int(2023), int(4), int(1)), DC_date__lte = date(int(2023), int(5), int(31)))
-        c = supply_return_fi.filter(company__company_name =  ab.company.company_name, company_goods__name = ab.company_goods.name, goods_company__goods_company_name = ab.goods_company.goods_company_name, DC_date__gte = date(int(2024), int(3), int(1)), DC_date__lte = date(int(2023), int(5), int(31)))
+        c = supply_return_fi.filter(company__company_name =  ab.company.company_name, company_goods__name = ab.company_goods.name, goods_company__goods_company_name = ab.goods_company.goods_company_name, DC_date__gte = date(int(2024), int(4), int(1)), DC_date__lte = date(int(2023), int(5), int(31)))
 
         x = 0
         y = 0
